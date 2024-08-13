6b66520dfcf1d32c584bfd0604569c5924d798c1
hcy6654 <hcy6654@gmail.com>
2024-08-13 23:01:06 +0900

    Home component
    Home component 개발 및 css 스타일링 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,41 @@ export default function Home() {
             <Link to="/profile">프로필</Link>
           </div>
         </header>
-        <div className="post-list">Post List</div>
+        <div className="post_navigation">
+          <div className="post_navigation-active">전체 글</div>
+          <div>나의 글</div>
+        </div>
+        <div className="post-list">
+          {[...Array(10)].map((e, index) => (
+            <div key={index} className="post_box">
+              <Link to={`posts/${index}`}>
+                <div className="post_profile-box">
+                  <div className="post_profile"></div>
+                  <div className="post_author-name">한지웅</div>
+                  <div className="post_date">2024.08.13</div>
+                </div>
+                <div className="post_title">게시글 {index}</div>
+                <div className="post_text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Nisl tincidunt eget nullam non. Quis hendrerit dolor magna
+                  eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris
+                  sit amet massa. Commodo odio aenean sed adipiscing diam donec
+                  adipiscing tristique. Mi eget mauris pharetra et. Non tellus
+                  orci ac auctor augue. Elit at imperdiet dui accumsan sit.
+                  Ornare arcu dui vivamus arcu felis. Egestas integer eget
+                  aliquet nibh praesent. In hac habitasse platea dictumst
+                  quisque sagittis purus. Pulvinar elementum integer enim neque
+                  volutpat ac.
+                </div>
+                <div className="post_utils-box">
+                  <div className="post_delete">삭제</div>
+                  <div className="post_edit">수정</div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
         <footer>
           <div>Menu 1</div>
           <div>Menu 2</div>
