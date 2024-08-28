90b7098212574b018a1e1c83ca7f4b808034feff
hcy6654 <hcy6654@gmail.com>
2024-08-28 23:33:17 +0900

    Firebase
    Firebase 기본설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { firebase, analytics } from "firebaseApp";
+
+console.log("firebase : " + firebase);
+console.log("analytics : " + analytics);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
